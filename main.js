c31e7097032db968c602a49c0c76db1b2516f7aa
Dan Mennella <dmennella34@gmail.com>
2023-12-25 11:32:04 -0500

    Refactor creep spawning logic and add comments

--- main.js
+++ main.js
@@ -5,17 +5,19 @@ var roleUpgrader = require("role.upgrader");
 var creepFunctions = require("creepFunctions");
 var roomPositionFunctions = require("roomPositionFunctions");
 
+// Function to calculate the body parts of a creep based on available energy in the room
 function getBody(segment, room) {
   let body = [];
-  // how much each sement cost
+  // Calculate the cost of a single segment of body parts
   let segmentCost = _.sum(segment, (s) => BODYPART_COST[s]);
 
-  // how much energey we can use total
+  // Retrieve the total energy capacity available in the room
   let energyAvailable = room.energyCapacityAvailable;
 
-  // how many times we can include the segment with room energy
+  // Determine the maximum number of segments that can be created with the available energy
   let maxSegments = Math.floor(energyAvailable / segmentCost);
 
+  // Construct the body parts array based on the maximum segments possible
   _.times(maxSegments, function () {
     _.forEach(segment, (s) => body.push(s));
   });
@@ -23,22 +25,28 @@ function getBody(segment, room) {
   return body;
 }
 
-// Export the main game loop
+// Main game loop
 module.exports.loop = function () {
-  // Iterate over all creeps in memory
+  // Iterate over all creeps stored in memory
   for (var name in Memory.creeps) {
-    // If a creep no longer exists in the game, remove it from memory
+    // Remove creeps from memory that no longer exist in the game
     if (!Game.creeps[name]) {
       delete Memory.creeps[name];
       console.log("Clearing non-existing creep memory:", name);
     }
   }
+
+  // Iterate over all rooms controlled by the player
   _.forEach(Game.rooms, function (room) {
+    // Check if the room has a controller and it belongs to the player
     if (room && room.controller && room.controller.my) {
+      // Determine the target number of harvesters for this room
       let harvesterTarget = _.get(room.memory, ["census", "harvester"], 4);
+      // Get the current number of harvester creeps
       var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == "harvester");
       console.log("Harvesters: " + harvesters.length);
 
+      // If there are fewer harvesters than the target, spawn new harvesters
       if (harvesters.length < harvesterTarget) {
         var newName = "Harvester" + Game.time;
         console.log("Spawning new harvester: " + newName);
@@ -46,6 +54,8 @@ module.exports.loop = function () {
           memory: { role: "harvester" },
         });
       }
+
+      // Similar logic for upgraders and builders
       let upgraderTarget = _.get(room.memory, ["census", "upgrader"], 4);
       var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == "upgrader");
       console.log("upgraders: " + upgraders.length);
@@ -60,8 +70,10 @@ module.exports.loop = function () {
       var builders = _.filter(Game.creeps, (creep) => creep.memory.role == "builder");
       console.log("builders: " + builders.length);
 
+      // Check for construction sites in the room
       let sites = room.find(FIND_CONSTRUCTION_SITES);
 
+      // Spawn new builders if there are construction sites and fewer builders than the target
       if (sites.length > 0 && builders.length < builderTarget) {
         var newName = "builder" + Game.time;
         console.log("Spawning new builder: " + newName);
@@ -70,6 +82,7 @@ module.exports.loop = function () {
     }
   });
 
+  // Display a visual notification when a creep is being spawned
   if (Game.spawns["Spawn1"].spawning) {
     var spawningCreep = Game.creeps[Game.spawns["Spawn1"].spawning.name];
     Game.spawns["Spawn1"].room.visual.text(
@@ -80,6 +93,7 @@ module.exports.loop = function () {
     );
   }
 
+  // Iterate over all creeps and execute their role-specific logic
   for (var name in Game.creeps) {
     var creep = Game.creeps[name];
     if (creep.memory.role == "harvester") {
