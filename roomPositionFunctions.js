555dcf4a6e77592ddc280ba1720674af6f15ef6c
Dan Mennella <dmennella34@gmail.com>
2023-12-24 23:10:37 -0500

    Refactor getNearbyPositions function to use block scope for loop variables

--- roomPositionFunctions.js
+++ roomPositionFunctions.js
@@ -3,8 +3,8 @@ RoomPosition.prototype.getNearbyPositions = function getNearbyFunctions() {
   let startX = this.x - 1 || 1;
   let startY = this.y - 1 || 1;
 
-  for (x = startX; x <= this.x && x < 49; x++) {
-    for (y = startY; y <= this.y + 1 && y < 49; y++) {
+  for (let x = startX; x <= this.x && x < 49; x++) {
+    for (let y = startY; y <= this.y + 1 && y < 49; y++) {
       if (x !== this.x || y !== this.y) {
         positions.push(new RoomPosition(x, y, this.roomName));
       }
