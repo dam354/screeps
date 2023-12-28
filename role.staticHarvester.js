5bc648789689d7cc858d809f11d57ffe2590ebfd
Dan Mennella <dmennella34@gmail.com>
2023-12-27 22:42:15 -0500

    Refactor staticHarvester role logic

--- role.staticHarvester.js
+++ role.staticHarvester.js
@@ -1,10 +1,15 @@
 var roleStaticHarvester = {
   run: function (creep) {
-    if (creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
-      if (creep.harvest(Game.getObjectById(creep.memory.sourceId)) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(Game.getObjectById(creep.memory.sourceId), { visualizePathStyle: { stroke: "#ffaa00" } });
-      }
-    } else {
+    // Attempt to harvest from the source
+    var harvestResult = creep.harvest(Game.getObjectById(creep.memory.sourceId));
+
+    // If not in range, move to the source
+    if (harvestResult == ERR_NOT_IN_RANGE) {
+      creep.moveTo(Game.getObjectById(creep.memory.sourceId), { visualizePathStyle: { stroke: "#ffaa00" } });
+    }
+
+    // If the creep is full, drop the energy
+    if (creep.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
       creep.drop(RESOURCE_ENERGY);
     }
   },
