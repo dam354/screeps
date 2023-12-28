74f0241c0c70e8d899a82d4b36cc993b3f5a23b9
Dan Mennella <dmennella34@gmail.com>
2023-12-28 17:12:37 -0500

    Refactor carrier and upgrader roles

--- role.carrier.js
+++ role.carrier.js
@@ -1,53 +1,30 @@
 // roleCarrier.js
 const roleCarrier = {
+  /** @param {Creep} creep **/
   run: function (creep) {
-    // Check if the creep needs to renew
+    // Creep renewal logic
     if (creep.ticksToLive < 50) {
       creep.say("Renewing");
       creep.moveTo(Game.spawns["Spawn1"]);
       Game.spawns["Spawn1"].renewCreep(creep);
-      return;
+      return; // Exit early to avoid further action this tick
     }
 
-    // Determine whether the creep should be collecting or delivering energy
-    if (creep.carry.energy < creep.carryCapacity && !creep.memory.delivering) {
-      // Energy Collection
-      let target = Game.getObjectById(creep.memory.targetId);
-      if (!target) {
-        const droppedEnergy = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
-          filter: (resource) => resource.resourceType == RESOURCE_ENERGY,
-        });
-
-        if (droppedEnergy) {
-          target = droppedEnergy;
-        } else {
-          target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0,
-          });
-        }
-
-        if (target) {
-          creep.memory.targetId = target.id;
-        } else {
-          creep.memory.targetId = null;
-        }
-      }
-
-      if (target) {
-        if (creep.pickup(target) == ERR_NOT_IN_RANGE || creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target, { visualizePathStyle: { stroke: "#ffaa00" } });
-        }
-      }
-    } else {
-      // Energy Delivery
+    if (creep.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
       creep.memory.delivering = true;
-      const target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+    } else if (creep.store[RESOURCE_ENERGY] === 0) {
+      creep.memory.delivering = false;
+    }
+    // Determine whether the creep should be collecting or delivering energy
+    if (creep.memory.delivering) {
+      // Delivering energy to a structure
+      let target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
         filter: (structure) => {
           return (
             (structure.structureType == STRUCTURE_SPAWN ||
               structure.structureType == STRUCTURE_EXTENSION ||
               structure.structureType == STRUCTURE_TOWER) &&
-            structure.energy < structure.energyCapacity
+            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
           );
         },
       });
@@ -57,15 +34,69 @@ const roleCarrier = {
           creep.moveTo(target, { visualizePathStyle: { stroke: "#ffffff" } });
         }
       } else {
+        // Switch to collecting mode if no delivery targets
         creep.memory.delivering = false;
+        creep.memory.targetId = null;
+      }
+    } else {
+      // Collecting energy from dropped resources or containers
+      let target = Game.getObjectById(creep.memory.targetId);
+
+      if (!target) {
+        target = this.findEnergyTarget(creep);
+        creep.memory.targetId = target ? target.id : null;
+      }
+
+      if (target) {
+        let actionResult = target instanceof Resource ? creep.pickup(target) : creep.withdraw(target, RESOURCE_ENERGY);
+        if (actionResult == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target, { visualizePathStyle: { stroke: "#ffaa00" } });
+        }
+      } else {
+        // If no targets are found, move to a standby position
+        this.moveToStandbyPosition(creep);
+      }
+    }
+  },
+
+  // Function to find targets, prioritizing distant ones if no nearby targets are found
+  findEnergyTarget: function (creep) {
+    let target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
+      filter: (resource) => resource.resourceType == RESOURCE_ENERGY,
+    });
+
+    if (!target) {
+      target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store.getUsedCapacity(RESOURCE_ENERGY) > 0,
+      });
+    }
+
+    // If no close target is found, look for the farthest one
+    if (!target) {
+      let allTargets = creep.room
+        .find(FIND_DROPPED_RESOURCES, {
+          filter: (resource) => resource.resourceType == RESOURCE_ENERGY,
+        })
+        .concat(
+          creep.room.find(FIND_STRUCTURES, {
+            filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store.getUsedCapacity(RESOURCE_ENERGY) > 0,
+          })
+        );
+
+      if (allTargets.length > 0) {
+        allTargets.sort((a, b) => creep.pos.getRangeTo(a) - creep.pos.getRangeTo(b));
+        target = allTargets[allTargets.length - 1]; // Select the farthest target
       }
     }
 
-    // Clear targetId when the task is complete or the target is no longer valid
-    if (creep.carry.energy == 0 || !Game.getObjectById(creep.memory.targetId)) {
-      creep.memory.targetId = null;
-      creep.memory.delivering = false;
-    }
+    return target;
+  },
+
+  // Logic for handling full spots near static harvesters
+  moveToStandbyPosition: function (creep) {
+    // Define standby positions or calculate them
+    const standbyPos = new RoomPosition(25, 25, creep.room.name); // Example standby position
+    creep.moveTo(standbyPos, { visualizePathStyle: { stroke: "#ffaa00" } });
   },
 };
 
--- role.upgrader.js
+++ role.upgrader.js
@@ -1,36 +1,34 @@
 var roleUpgrader = {
-  /** @param {Creep} creep **/
   run: function (creep) {
     // Switch states between working and harvesting
     if (creep.memory.working && creep.store[RESOURCE_ENERGY] == 0) {
       creep.memory.working = false;
-      creep.memory.path = null; // Clear the cached path
       creep.say("🌾 harvest");
     }
     if (!creep.memory.working && creep.store.getFreeCapacity() == 0) {
       creep.memory.working = true;
-      creep.memory.path = null; // Clear the cached path
       creep.say("📈 upgrade");
     }
 
     if (creep.memory.working) {
-      if (!creep.memory.path) {
-        // Generate and cache the path
-        creep.memory.path = creep.room.findPath(creep.pos, creep.room.controller.pos, {
-          ignoreCreeps: true,
-        });
-      }
+      // Attempt to upgrade the controller
       let upgradeResult = creep.upgradeController(creep.room.controller);
+
+      // Move to the controller if not in range
       if (upgradeResult == ERR_NOT_IN_RANGE) {
-        creep.moveByPath(creep.memory.path);
+        creep.moveTo(creep.room.controller, {
+          visualizePathStyle: { stroke: "#ffffff" },
+          reusePath: 5, // Reuse the path for 5 ticks
+        });
       } else if (upgradeResult == OK) {
-        // Build or repair roads on the creep's path
+        // Maintain the existing behavior for building roads
         if (creep.pos.lookFor(LOOK_STRUCTURES).filter((s) => s.structureType == STRUCTURE_ROAD).length == 0) {
           creep.pos.createConstructionSite(STRUCTURE_ROAD);
         }
       }
     } else {
-      creep.harvestEnergy(); // Make sure this function is efficient as well
+      // Harvest energy using the existing function or logic
+      creep.harvestEnergy();
     }
   },
 };
