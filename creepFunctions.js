03b379770b71f275acf8b4c76a952a97ffe7d27b
Dan Mennella <dmennella34@gmail.com>
2023-12-29 14:19:03 -0500

    Refactor energy harvesting logic in Creep.prototype.harvestEnergy()

--- creepFunctions.js
+++ creepFunctions.js
@@ -18,38 +18,22 @@ Creep.prototype.findEnergySource = function () {
 };
 
 Creep.prototype.harvestEnergy = function harvestEnergy() {
-  // let storedSource = Game.getObjectById(this.memory.source);
-  // // Validate the stored source or find a new one
-  // if (!storedSource || (storedSource.pos.getOpenPositions().length === 0 && !this.pos.isNearTo(storedSource))) {
-  //   delete this.memory.source;
-  //   storedSource = this.findEnergySource();
-  // }
-  // // Perform the harvest operation
-  // if (storedSource) {
-  //   const harvestResult = this.harvest(storedSource);
-  //   if (harvestResult === ERR_NOT_IN_RANGE) {
-  //     this.moveTo(storedSource, {
-  //       visualizePathStyle: { stroke: "#ffaa00" },
-  //       reusePath: 5,
-  //     });
-  //   }
-  // }
   if (this.carry.energy < this.carryCapacity) {
-    const droppedEnergy = this.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
-      filter: (resource) => resource.resourceType == RESOURCE_ENERGY,
+    const container = this.pos.findClosestByPath(FIND_STRUCTURES, {
+      filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0,
     });
 
-    if (droppedEnergy) {
-      if (this.pickup(droppedEnergy) == ERR_NOT_IN_RANGE) {
-        this.moveTo(droppedEnergy, { visualizePathStyle: { stroke: "#ffaa00" }, reusePath: 20 });
-      }
+    if (container && this.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      this.moveTo(container, { visualizePathStyle: { stroke: "#ffaa00" },reusePath: 20 });
     } else {
-      const container = this.pos.findClosestByPath(FIND_STRUCTURES, {
-        filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0,
+      const droppedEnergy = this.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
+        filter: (resource) => resource.resourceType == RESOURCE_ENERGY,
       });
 
-      if (container && this.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        this.moveTo(container, { visualizePathStyle: { stroke: "#ffaa00" },reusePath: 20 });
+      if (droppedEnergy) {
+        if (this.pickup(droppedEnergy) == ERR_NOT_IN_RANGE) {
+          this.moveTo(droppedEnergy, { visualizePathStyle: { stroke: "#ffaa00" }, reusePath: 20 });
+        }
       }
     }
   }
