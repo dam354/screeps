62eb525bc79aa2220ec0245d0e0d0c654f2170d5
Dan Mennella <dmennella34@gmail.com>
2023-12-28 20:25:31 -0500

    Update role.carrier.js to include containers in target selection

--- role.carrier.js
+++ role.carrier.js
@@ -22,7 +22,8 @@ const roleCarrier = {
       let target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
         filter: (structure) => {
           return (
-            (structure.structureType == STRUCTURE_SPAWN ||
+            (structure.structureType == STRUCTURE_CONTAINER ||
+              structure.structureType == STRUCTURE_SPAWN ||
               structure.structureType == STRUCTURE_EXTENSION ||
               structure.structureType == STRUCTURE_TOWER) &&
             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
