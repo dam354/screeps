301d236d5cc1abd7015ddb517f33bc03d72063f0
Dan Mennella <dmennella34@gmail.com>
2023-12-29 14:08:46 -0500

    Refactor carrier role to prioritize non-spawn structures for energy delivery

--- role.carrier.js
+++ role.carrier.js
@@ -23,14 +23,19 @@ const roleCarrier = {
       let target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
         filter: (structure) => {
           return (
-            (structure.structureType == STRUCTURE_CONTAINER ||
-              structure.structureType == STRUCTURE_SPAWN ||
+            (structure.structureType == STRUCTURE_SPAWN ||
               structure.structureType == STRUCTURE_EXTENSION ||
               structure.structureType == STRUCTURE_TOWER) &&
             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
           );
         },
       });
+
+      if (!target) {
+        target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+          filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0,
+        });
+      }
 
       if (target) {
         if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
