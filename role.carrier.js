ad6be59a68c740fc28a8d82d3e9128506b1e92b2
Dan Mennella <dmennella34@gmail.com>
2023-12-29 12:46:26 -0500

    Refactor energy capacity check in roleCarrier.js

--- role.carrier.js
+++ role.carrier.js
@@ -27,7 +27,7 @@ const roleCarrier = {
               structure.structureType == STRUCTURE_SPAWN ||
               structure.structureType == STRUCTURE_EXTENSION ||
               structure.structureType == STRUCTURE_TOWER) &&
-            (structure.store.getCapacity(RESOURCE_ENERGY) - structure.store[RESOURCE_ENERGY]) > 0
+            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
           );
         },
       });
